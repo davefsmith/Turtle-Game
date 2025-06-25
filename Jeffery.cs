79f5acfdc78120b40b3d1f68c0c22a8424fcb4e1
David Smith <47852753+davefsmith@users.noreply.github.com>
2025-06-25 01:46:53 +0100

    Jeffery's Got Momentum Now

--- Jeffery.cs
+++ Jeffery.cs
@@ -4,7 +4,9 @@ using System;
 public partial class Jeffery : Area2D
 {
 	[Export]
-	public int Speed {get; set;} = 100;
+	public int Acc {get; set;} = 100;
+	[Export]
+	public Vector2 Velocity = Vector2.Zero;
 	public Vector2 ScreenSize;
 
 
@@ -15,35 +17,41 @@ public partial class Jeffery : Area2D
 
 	public override void _Process(double delta)
 	{
-		var velocity = Vector2.Zero; // The player's movement vector.
+		var movementAcc = Vector2.Zero; // The player's movement vector.
 		if (Input.IsActionPressed("move_right"))
 		{
-			velocity.X += 1;
+			movementAcc.X += 1;
 		}
 		if (Input.IsActionPressed("move_left"))
 		{
-			velocity.X -= 1;
+			movementAcc.X -= 1;
 		}
 		if (Input.IsActionPressed("move_down"))
 		{
-			velocity.Y += 1;
+			movementAcc.Y += 1;
 		}
 		if (Input.IsActionPressed("move_up"))
 		{
-			velocity.Y -= 1;
+			movementAcc.Y -= 1;
 		}
+		movementAcc = movementAcc.Normalized() * Acc;
+		Vector2 drag = Velocity.Sign() * -0.5f * Velocity.LengthSquared() / Acc;
+		
 		var animatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
-		if (velocity.Length() > 0)
+		
+		if (movementAcc.LengthSquared() > 0)
 		{
-			velocity = velocity.Normalized() * Speed;
 			animatedSprite2D.Play();
-			Rotation = velocity.Angle() + 0.7156f;
+			animatedSprite2D.SetSpeedScale((movementAcc.LengthSquared()-drag.LengthSquared())/(Acc*Acc)+0.1f);
+			Rotation = movementAcc.Angle() + 0.7156f;
 		}
 		else
 		{
 			animatedSprite2D.Stop();
 		}
-		Position += velocity * (float)delta;
+		
+		Velocity += (movementAcc + drag) * (float)delta;
+		Position += Velocity * (float)delta;
 		Position = new Vector2(	x: Mathf.Clamp(Position.X, 0, ScreenSize.X), y: Mathf.Clamp(Position.Y, 0, ScreenSize.Y));
 	}
 }
